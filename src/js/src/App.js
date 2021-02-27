9a081a06931b079179168a2d1063f9eae46c26fe
bashiryousufy <bashiryousufy@gmail.com>
2021-02-27 23:56:38 +0800

    consuming API

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -2,11 +2,9 @@ import "./App.css";
 import { getAllStudents } from "./client";
 
 function App() {
-  getAllStudents().then((res) =>
-    res.json().then((students) => {
-      console.log(students);
-    })
-  );
+  getAllStudents()
+    .then((res) => res.json())
+    .then(console.log);
   return <h1>Hello world Spring boot and React</h1>;
 }
 
